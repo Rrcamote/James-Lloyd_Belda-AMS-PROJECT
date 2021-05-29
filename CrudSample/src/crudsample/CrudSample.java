0796565081e1a95c5b416dcf2a250867e1eef69b
Romeo Rodemio <romeo.rodemio@gmail.com>
2021-05-29 08:28:13 +0800

    FINAL AMS JAVA

---
+++ CrudSample/src/crudsample/CrudSample.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package crudsample;
+
+/**
+ *
+ * @author 2ndyrGroupA
+ */
+public class CrudSample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       new mainFrame().setVisible(true);
+       new Login().setVisible(true);
+   
+    }  
+}
